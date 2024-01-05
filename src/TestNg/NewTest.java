87abcd21ea53c64491e369b5aba2f70f98ade128
anupr <anu.prg09@gmail.com>
2024-01-05 08:14:44 +0530

    this is a demo project to Jenkins

---
+++ src/TestNg/NewTest.java
@@ -0,0 +1,32 @@
+package TestNg;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	WebDriver driver;
+	
+  @Test
+  public void MyTest() {
+	  driver.navigate().to("https://www.google.com");
+	  assertEquals("true", "true");
+  }
+
+  @BeforeTest
+  public void StartBrowser() {
+	  driver = new ChromeDriver();
+	  driver.manage().window();
+  }
+
+  @AfterTest
+  public void closeBrowser() {
+	  driver.close();
+  }
+
+}
